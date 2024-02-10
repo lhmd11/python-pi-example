5caa2ef7dc3df71721709fc928107015df6a9995
lhmd <lhmd11@gmail.com>
2024-02-10 08:35:45 +0100

    2 change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+# Adding a  2 different comment
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
